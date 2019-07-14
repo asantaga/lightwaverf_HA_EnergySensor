59b3a2d92a64d1aa771ea51fe8cc79b20ff5335d
angelo santagata <angelosantagata@gmail.com>
2019-07-15 00:52:46 +0100

    fixed, getting 2 instances of platforms... still tbd

---
+++ lightwaverf_energy/__init__.py
@@ -0,0 +1,29 @@
+"""
+Sensor Platform Device for LightwaveRF Electricity Monitor.
+
+https://github.com/asantaga/wiserHomeAssistantPlatform
+Angelo.santagata@gmail.com
+"""
+
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+from homeassistant.const import (CONF_HOST, CONF_MINIMUM, CONF_PASSWORD,
+                                 CONF_SCAN_INTERVAL)
+from homeassistant.helpers.discovery import load_platform
+
+DOMAIN = 'lightwaverf_energy'
+DATA_KEY = 'lightwaverf_energy'
+
+PLATFORM_SCHEMA = vol.Schema({
+
+    vol.Optional(CONF_SCAN_INTERVAL, default=300): cv.time_period,
+    vol.Optional(CONF_MINIMUM, default=-5): vol.All(vol.Coerce(int))
+})
+
+
+def setup(hass, config):
+
+    # Store the scan interval in the data storage.
+    hass.data[DATA_KEY] = config[DOMAIN][CONF_SCAN_INTERVAL]
+    load_platform(hass, 'sensor', DOMAIN, {}, config)
+    return True
